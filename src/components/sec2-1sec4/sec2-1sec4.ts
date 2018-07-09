8998dd0f907bc6e7fb31a4255b45d065dfb1874a
Pee2539 <pyakuza77@gmail.com>
2018-07-09 17:14:03 +0700

    Add sec2-1sec4

---
+++ src/components/sec2-1sec4/sec2-1sec4.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+
+/**
+ * Generated class for the Sec2_1sec4Component component.
+ *
+ * See https://angular.io/api/core/Component for more info on Angular
+ * Components.
+ */
+@Component({
+  selector: 'sec2-1sec4',
+  templateUrl: 'sec2-1sec4.html'
+})
+export class Sec2_1sec4Component {
+
+  text: string;
+
+  constructor() {
+    console.log('Hello Sec2_1sec4Component Component');
+    this.text = 'Hello World';
+  }
+
+}
