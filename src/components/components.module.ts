f5dabfe8a3b68eb00c19bf97e763ab4c39056214
sinthuC <sinthu_ch@kkumail.com>
2018-07-09 11:47:38 +0700

    INIT module component.module INTO app.module

---
+++ src/components/components.module.ts
@@ -0,0 +1,9 @@
+import { NgModule } from '@angular/core';
+import { IonicModule } from 'ionic-angular';
+import { Sn2_1sec1Component } from './sn2-1sec1/sn2-1sec1';
+@NgModule({
+	declarations: [Sn2_1sec1Component],
+	imports: [IonicModule],
+	exports: [Sn2_1sec1Component]
+})
+export class ComponentsModule {}
