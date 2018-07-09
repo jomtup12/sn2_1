b5de6f17560d06a9ec16858175ea631af672145e
sinthuC <sinthu_ch@kkumail.com>
2018-07-09 11:47:08 +0700

    ADD component sn2-1sec1

---
+++ src/components/sn2-1sec1/sn2-1sec1.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+
+/**
+ * Generated class for the Sn2_1sec1Component component.
+ *
+ * See https://angular.io/api/core/Component for more info on Angular
+ * Components.
+ */
+@Component({
+  selector: 'sn2-1sec1',
+  templateUrl: 'sn2-1sec1.html'
+})
+export class Sn2_1sec1Component {
+
+  text: string;
+
+  constructor() {
+    console.log('Hello Sn2_1sec1Component Component');
+    this.text = 'Hello World';
+  }
+
+}
