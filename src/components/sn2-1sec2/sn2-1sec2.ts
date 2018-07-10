9cdc7dd0d86fc0867c8b928eb47efc84de395f2e
sinthuC <sinthu_ch@kkumail.com>
2018-07-10 16:16:54 +0700

    UPDATE component sn2-1sec2

--- src/components/sn2-1sec2/sn2-1sec2.ts
+++ src/components/sn2-1sec2/sn2-1sec2.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup,FormBuilder,FormArray } from '@angular/forms';
+import { FormGroup, FormBuilder, FormArray } from '@angular/forms';
 
 /**
  * Generated class for the Sn2_1sec2Component component.
@@ -12,58 +12,92 @@ import { FormGroup,FormBuilder,FormArray } from '@angular/forms';
   templateUrl: 'sn2-1sec2.html'
 })
 export class Sn2_1sec2Component {
-  area1 :any;
-  sec2Form : FormGroup
-  s2Data:any[] = [];
-  currentPage:any;
+  area1: any;
+  sec2Form: FormGroup
+  s2Data: any[] = [];
+  currentPage: any;
   constructor(private fb: FormBuilder) {
-  
-    this.currentPage="1";
+
+    this.currentPage = "1";
   }
 
 
-  ngOnInit(){
+  ngOnInit() {
     this.sec2Form = this.fb.group({
       's1': '',
       's2': null,
-      'cPage':1,
+      'cPage': null,
       's2Data': this.fb.array([]),
 
     });
   }
 
-  pushToArray(length:any){
+  pushToArray(length: any) {
 
     const s2 = this.sec2Form.get('s2Data');
-    for(let i=0;i<length;i++){
-    (s2 as FormArray).push(this.fb.group({
-      's2_1':'',
-      's2_2':'',
-      's2_3':'',
-      's2_4':this.fb.array([]),
-      's2_5':'',
-      's2_6':'',
+    let isNegative = false;
+    console.log(s2.value.length);
+    if (length > s2.value.length) {
+      length = length - s2.value.length
+    } else if (length < s2.value.length) {
+      length = s2.value.length - length
+      isNegative = true;
+    }
+    for (let i = 0; i < length; i++) {
+      if (!isNegative) {
+        (s2 as FormArray).push(this.fb.group({
+          's2_1': '',
+          's2_2': '',
+          's2_3': '',
+          's2_4': this.fb.array([]),
+          's2_5': '',
+          's2_6': '',
 
-    }));
+        }));
+      }
+      else {
+        (s2 as FormArray).removeAt(s2.value.length - 1);
+      }
+    }
+    this.sec2Form.patchValue({
+      cPage: 1,
+    });
+
   }
-  console.log(this.sec2Form.get('s2Data'));
 
-  }
+  pushToSubArray(index: number, length: any) {
+    const s2Sub = this.sec2Form.get(['s2Data', index, 's2_4']);
+    let isNegative = false;
 
-  pushToSubArray(index:number,length:any){
-    const s2Sub = this.sec2Form.get(['s2Data',index,'s2_4']);
-    for(let i =0 ;i <length;i++){
-      (s2Sub as FormArray).push(this.fb.group({
-        's3_1':'',
-        's3_2':'',
-      }));
+    if (length > s2Sub.value.length) {
+      length = length - s2Sub.value.length
+    } else if (length < s2Sub.value.length) {
+      length = s2Sub.value.length - length
+      isNegative = true;
+    }
+    for (let i = 0; i < length; i++) {
+      if (!isNegative) {
+        (s2Sub as FormArray).push(this.fb.group({
+          's3_1': '',
+          's3_2': '',
+        }));
+      } else {
+        (s2Sub as FormArray).removeAt(s2Sub.value.length - 1);
+      }
     }
 
   }
 
-  changeMenus1(i:any){
+  changeMenus1(i: any) {
     this.sec2Form.patchValue({
-      cPage : i+1,
+      cPage: i + 2,
+    });
+  }
+
+  resetForm() {
+    this.sec2Form.reset()
+    this.sec2Form.patchValue({
+      s1: false,
     });
   }
 }
