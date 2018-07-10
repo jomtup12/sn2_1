851b4ff3790098e61c07485cf240e52cb394803b
sinthuC <sinthu_ch@kkumail.com>
2018-07-10 13:41:31 +0700

    UPDATE component sn2-1sec2

--- src/components/sn2-1sec2/sn2-1sec2.ts
+++ src/components/sn2-1sec2/sn2-1sec2.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormGroup,FormBuilder,FormArray } from '@angular/forms';
 
 /**
@@ -17,20 +17,20 @@ export class Sn2_1sec2Component {
   s2Data:any[] = [];
   currentPage:any;
   constructor(private fb: FormBuilder) {
-    this.createForm();
-    this.currentPage=1;
+  
+    this.currentPage="1";
   }
 
-  createForm(){
+
+  ngOnInit(){
     this.sec2Form = this.fb.group({
       's1': '',
       's2': null,
-      'currentPage': 1,
+      'cPage':1,
       's2Data': this.fb.array([]),
 
     });
   }
-
 
   pushToArray(length:any){
 
@@ -40,16 +40,30 @@ export class Sn2_1sec2Component {
       's2_1':'',
       's2_2':'',
       's2_3':'',
-      's2_4':'',
+      's2_4':this.fb.array([]),
       's2_5':'',
+      's2_6':'',
 
     }));
   }
+  console.log(this.sec2Form.get('s2Data'));
+
+  }
+
+  pushToSubArray(index:number,length:any){
+    const s2Sub = this.sec2Form.get(['s2Data',index,'s2_4']);
+    for(let i =0 ;i <length;i++){
+      (s2Sub as FormArray).push(this.fb.group({
+        's3_1':'',
+        's3_2':'',
+      }));
+    }
+
   }
 
   changeMenus1(i:any){
     this.sec2Form.patchValue({
-      currentPage : i+1,
+      cPage : i+1,
     });
   }
 }
